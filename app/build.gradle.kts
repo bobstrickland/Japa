afc41746a25d2839c54de47cc4c120d0ac87e388
Bob Strickland <bob.sansmal@gmail.com>
2026-04-14 17:01:31 -0400

    incrementing build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "org.strickland.japa"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
